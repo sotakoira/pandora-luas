75610f6d51f2e163b3dd28ee8ed137b47a81538e
sotakoira <63243610+sotakoira@users.noreply.github.com>
2021-02-13 03:05:35 +0100

    optimization

--- flash_alpha.lua
+++ flash_alpha.lua
@@ -1,11 +1,16 @@
 -- https://github.com/sotakoira/pandora-luas
+ui.add_label("Works only after you get flashed for the first time on a map.")
 local alpha_slider = ui.add_slider("Flash alpha", 1, 255)
 
-function main()
-if engine.is_connected() then
+function on_throw(event)
+weapon = event:get_string("weapon")
+if weapon == "flashbang" then
 local_player = entity_list.get_client_entity(engine.get_local_player())
-local_player:get_prop("DT_CSPlayer", "m_flFlashMaxAlpha"):set_float(alpha_slider:get())
+flFlashMaxAlpha = local_player:get_prop("DT_CSPlayer", "m_flFlashMaxAlpha")
+if flFlashMaxAlpha:get_float() ~= alpha_slider:get() then
+flFlashMaxAlpha:set_float(alpha_slider:get())
+end
 end
 end
 
-callbacks.register("paint", main)
+callbacks.register("grenade_thrown", on_throw)
